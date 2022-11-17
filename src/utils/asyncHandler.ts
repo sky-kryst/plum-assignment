b89b7a2a6e1e5085aa4967ea95d5045b196a33f2
sky-kryst <aakashmeshram@outlook.com>
2022-11-17 15:39:10 +0530

    refactor(repo): :rocket: code optimizations

--- src/utils/asyncHandler.ts
+++ src/utils/asyncHandler.ts
@@ -1,8 +1,8 @@
-import { NextFunction, Request, RequestHandler, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 
 export const AsyncHandler = (
   // Takes a RequestHandler function and an optional defaultError config
-  fn: RequestHandler,
+  fn: (req: Request, res: Response, next: NextFunction) => Promise<any>,
   defaultError?: { code?: number; message?: string; status?: string }
 ) => {
   // returns a function that calls the given function, handles it's error
